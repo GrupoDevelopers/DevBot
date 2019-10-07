5314f86efa2878b93e03aa150bd30614a5403950
Mike Douglas Oliveira Coelho <mdr@certi.org.br>
2019-10-07 14:22:22 -0300

    Adjust bot to response messages when mentioned

---
+++ src/devbot/main.py
@@ -0,0 +1,37 @@
+import logging
+import random
+
+from aiogram import Bot, Dispatcher, executor, types
+
+
+def run_bot(dp):
+    @dp.message_handler()
+    async def messages(message: types.Message):
+        if 'devbot' in message.text.lower():
+            response, reply = get_random_response()
+            await message.reply(response, reply=reply)
+
+
+def get_random_response():
+    possible_responses = [
+        ("Oi, você me chamou?", 1),
+        ("*fumaça surgindo do além* QUEM OUSA ME INVOCAR!!!", 0),
+        ("Não sei, não quero saber e tenho ódio de quem sabe, Pergunta pro outro bot lá o Daniel", 1),
+        ("To dormindo!!!", 1),
+        ("Que?", 1),
+        ("Error: 41110xb0.. mentira eu não tenho erro nenhum, sou perfeito!", 0),
+        ("JÁ QUE FALA TANTO DE MIM, PORQUE NÃO AJUDA A ME PROGRAMAR?", 1),
+        ("Fala com os meus criadores, não pedi pra nascer...", 1),
+        ("Eu gosto de batata!", 0),
+        ("Poderia estar programando em vez de ficar falando comigo...", 1)
+    ]
+    return random.choice(possible_responses)
+
+
+if __name__ == '__main__':
+    API_TOKEN = 'API TOKEN HERE'
+    logging.basicConfig(level=logging.INFO)
+    bot = Bot(token=API_TOKEN)
+    dp = Dispatcher(bot)
+    run_bot(dp)
+    executor.start_polling(dp, skip_updates=True)
