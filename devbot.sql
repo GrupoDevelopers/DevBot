90817ee4a85560c9cd828db947a73a8007dfcaf2
caiomuniz <caiocsmuniz1@gmail.com>
2020-02-13 20:29:32 -0300

    created 'subscribed_tag' and 'tag' tables

--- devbot.sql
+++ devbot.sql
@@ -1,106 +1,89 @@
--- MySQL dump 10.13  Distrib 5.7.28, for Linux (x86_64)
---
--- Host: localhost    Database: devbot
--- ------------------------------------------------------
--- Server version	5.7.28-0ubuntu0.18.04.4
+-- MySQL Workbench Forward Engineering
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
---
--- Table structure for table `chats`
---
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema devbot
+-- -----------------------------------------------------
 
-DROP TABLE IF EXISTS `chats`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `chats` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `chat_id` bigint(20) DEFAULT NULL,
-  `title` varchar(255) NOT NULL,
-  `chat_type` varchar(45) NOT NULL,
+-- -----------------------------------------------------
+-- Schema devbot
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `devbot` ;
+USE `devbot` ;
+
+-- -----------------------------------------------------
+-- Table `devbot`.`chats`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `devbot`.`chats` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `chat_id` BIGINT NULL DEFAULT NULL,
+  `title` VARCHAR(255) NOT NULL,
+  `chat_type` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `chat_id_UNIQUE` (`chat_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  UNIQUE INDEX `chat_id_UNIQUE` (`chat_id` ASC))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
 
---
--- Dumping data for table `chats`
---
 
-LOCK TABLES `chats` WRITE;
-/*!40000 ALTER TABLE `chats` DISABLE KEYS */;
-/*!40000 ALTER TABLE `chats` ENABLE KEYS */;
-UNLOCK TABLES;
+-- -----------------------------------------------------
+-- Table `devbot`.`experiences`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `devbot`.`experiences` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_telegram_id` BIGINT NOT NULL,
+  `chat_id` BIGINT NOT NULL,
+  `experience_points` INT NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
 
---
--- Table structure for table `experiences`
---
 
-DROP TABLE IF EXISTS `experiences`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `experiences` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_telegram_id` bigint(20) NOT NULL,
-  `chat_id` bigint(20) NOT NULL,
-  `experience_points` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- -----------------------------------------------------
+-- Table `devbot`.`subscribed_tag`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `devbot`.`subscribed_tag` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `id_user` INT NOT NULL,
+  `id_tag` INT NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
 
---
--- Dumping data for table `experiences`
---
 
-LOCK TABLES `experiences` WRITE;
-/*!40000 ALTER TABLE `experiences` DISABLE KEYS */;
-/*!40000 ALTER TABLE `experiences` ENABLE KEYS */;
-UNLOCK TABLES;
+-- -----------------------------------------------------
+-- Table `devbot`.`tag`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `devbot`.`tag` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `tag` VARCHAR(255) NOT NULL,
+  `approved_by` VARCHAR(45) NULL DEFAULT NULL,
+  `status` VARCHAR(45) NOT NULL DEFAULT 'Aguardando aprovacao',
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
 
---
--- Table structure for table `users`
---
 
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `telegram_id` bigint(20) DEFAULT NULL,
-  `is_bot` int(11) NOT NULL,
-  `first_name` varchar(255) COLLATE utf8_bin NOT NULL,
-  `last_name` varchar(255) CHARACTER SET utf8 NOT NULL,
-  `username` varchar(255) CHARACTER SET utf8 NOT NULL,
+-- -----------------------------------------------------
+-- Table `devbot`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `devbot`.`users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `telegram_id` BIGINT NULL DEFAULT NULL,
+  `is_bot` INT NOT NULL,
+  `first_name` VARCHAR(255) CHARACTER SET 'utf8' NOT NULL,
+  `last_name` VARCHAR(255) CHARACTER SET 'utf8' NOT NULL,
+  `username` VARCHAR(255) CHARACTER SET 'utf8' NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `telegram_id_UNIQUE` (`telegram_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  UNIQUE INDEX `telegram_id_UNIQUE` (`telegram_id` ASC))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_bin;
 
---
--- Dumping data for table `users`
---
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2020-01-02 20:35:50
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
