e7a7eeb845b1d608899287b16e9c4ec671d62e1a
VYCKTOR STARK <huho387@gmail.com>
2020-07-23 23:54:18 +0000

    Changing the way to call the Bot method
    I am changing, because this method is not only used to place a bot token, there are several functions associated with it, that is, we need to leave it in an easily accessible place to call when needed

--- src/dev_bot.py
+++ src/dev_bot.py
@@ -8,16 +8,18 @@
 
 import logging
 
-from aiogram import types
+from aiogram import types, Bot
 
 from bot_behaviors.random_responses import random_response
 from bot_behaviors.experience import Experience
 from database.database import Database
-
+from decouple import config
+bot = Bot(token= config('API_TOKEN'))
 
 class DevBot:
     def __init__(self, dispatcher):
         self.dispatcher = dispatcher
+        self.bot = bot
         self.database = Database()
         self.experience = Experience(self.database)
         self.run_bot()
