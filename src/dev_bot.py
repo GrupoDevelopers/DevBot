68806af936f2b7e221a34b8434362124c595e557
Vycktor Stark <huho387@gmail.com>
2020-08-14 10:15:09 -0400

    fixing issues to find deleted account names + fixing surname issues together

--- src/dev_bot.py
+++ src/dev_bot.py
@@ -26,10 +26,13 @@ class DevBot:
 
     def run_bot(self):
         async def check_name_member(chatid, userid):
-            member = await bot.get_chat_member(chat_id=str(chatid), user_id=str(userid))
-            name_member = member['user']['first_name']
-            if ("last_name" in member['user']): name_member += member['user']['last_name']
-            return name_member
+            try:
+                member = await bot.get_chat_member(chat_id=str(chatid), user_id=str(userid))
+                name_member = member['user']['first_name']
+                if ("last_name" in member['user']): f"{name_member} {member['user']['last_name']}"
+                return name_member
+            except:
+                return "Conta Excluida"
             
         @self.dispatcher.message_handler(commands=['exp'])
         async def exp(message: types.Message):
